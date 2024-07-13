a44d6eaeb37db633a032d393ed0c5b787a28b44a
ali-shokor <130291318+ali-shokor@users.noreply.github.com>
2024-07-13 19:17:28 +0300

    script.js

--- script.js
+++ script.js
@@ -1,42 +1,3 @@
-var inputBox = document.getElementById("input-box");
-var listContainer = document.getElementById("list");
-
-function addTask(){
-    if (inputBox.value === ""){
-        alert("You should write something!")
-    }
-    else{
-        var li = document.createElement("li")
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        var span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = "";
-    saveData();
-}
-
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove()
-        saveData();
-    }
-}, false);
-
-function saveData(){
-    localStorage.setItem("data2", listContainer.innerHTML);
-}
-
-function getTask(){
-    listContainer.innerHTML = localStorage.getItem("data2");
-}
-getTask();
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "YOUR_API_KEY",
@@ -49,17 +10,42 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-
 const db = firebase.firestore();
 
-// Adding data
-db.collection("tasks").add({
-    task: "Example task",
-    completed: false
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
+var inputBox = document.getElementById("input-box");
+var listContainer = document.getElementById("list");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You should write something!");
+    } else {
+        var li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        var span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        saveData();
+    }
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data2", listContainer.innerHTML);
+}
+
+function getTask() {
+    listContainer.innerHTML = localStorage.getItem("data2");
+}
+
+getTask();
